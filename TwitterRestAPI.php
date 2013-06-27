ea2cd47382cbdbf5aefde08e2e5dbf336ce75cd5
Ben Masters <mijahn.ben@gmail.com>
2013-06-27 16:20:47 +0100

    credit where its due

--- TwitterRestAPI.php
+++ TwitterRestAPI.php
@@ -12,7 +12,7 @@
     |  |   |  |('  '-'(_.-'   |  |     |  | |  ||  | \   |     |  |          |      |   |  | |  | | '--'  /\       /
     `--'   `--'  `-----'      `--'     `--' `--'`--'  `--'     `--'          `------'   `--' `--' `------'  `-----'
 
- * Coder: mijahn
+ * Coder: Mijahn - with help from Rich
  * Date: 27/06/13
  * Time: 11:53
  *
@@ -34,17 +34,17 @@ class TwitterRestAPI extends Server {
         $this->twitter = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET);
     }
 
-    public function getUserStatus() {
-        return $this->twitter->get('statuses/user_timeline');
-    }
-
-    //override
+    //@override from RestService\Server
     public function send($pData)
     {
         return $this->getClient()->sendResponse(200, array('data' => $pData)); // override so you can change 'data' to whatever you like on the success method
     }
 
-    public function getCachedUserStatus() {
+    public function getUserStatus() { // access this directly for up to the second tweets - will use a twitter API request.
+        return $this->twitter->get('statuses/user_timeline');
+    }
+
+    public function getCachedUserStatus() { // to save your twitter api limit getting hammered
         if (file_exists('twitter_result.data')) {
             $data = unserialize(file_get_contents('twitter_result.data'));
             if ($data['timestamp'] > time() - 10 * 60) {
@@ -57,7 +57,6 @@ class TwitterRestAPI extends Server {
             $data = array ('twitter_result' => $twitter_result, 'timestamp' => time());
             file_put_contents('twitter_result.data', serialize($data));
         }
-
         return $twitter_result;
 
     }
